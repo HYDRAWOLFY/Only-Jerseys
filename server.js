cab4b35309787ea577d4f63f013e8847be3a06a4
Rajeev Negi <rajeevnegi005@gmail.com>
2025-09-27 20:07:23 +0530

    improved file structure, added login and reg page

--- server.js
+++ server.js
@@ -2,19 +2,26 @@ const express = require("express");
 const app = express();
 const port = 3000;
 
-
 app.listen(port,()=>{
     console.log(`listening on port ${port}`);
 });
 
+// middleware
+app.use(express.static(`${__dirname}/public`));
+
 // homepage route
 app.get('/',(req,res)=>{
-    res.send(200);
+    // res.send(200);
+    res.sendFile(__dirname + "/index.html");
 });
 
 // login page route
 app.get('/login',(req,res)=>{
     res.sendFile(__dirname + "/login.html");
+});
+
+app.post('/login',(req,res)=>{
+    res.send("Posted!");
 });
 
 // register page route
